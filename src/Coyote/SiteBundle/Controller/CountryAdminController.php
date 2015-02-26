91db1ac12ae0a65a093bb2194507768c04fcaace
Coyote <coyotealone@hotmail.fr>
2015-02-26 15:22:46 +0100

    1.15.6
    Update SonataAdmin
    Fix errors Schedule

---
+++ src/Coyote/SiteBundle/Controller/CountryAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Coyote\SiteBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class CountryAdminController extends CRUDController
+{
+
+}
---
+++ src/Coyote/SiteBundle/Controller/SiteAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Coyote\SiteBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class SiteAdminController extends CRUDController
+{
+
+}
