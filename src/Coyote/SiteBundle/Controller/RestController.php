680cfd9e52386060413162c74ab531a37ad30a8c
Coyote <coyotealone@hotmail.fr>
2015-03-06 20:22:51 +0100

    1.18.0
    Add FosRestBundle / ApiDoc
    Add routing api/doc
    Update Symfony 2.6

---
+++ src/Coyote/SiteBundle/Controller/RestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Coyote\SiteBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class RestController extends FOSRestController
+{
+    
+}
