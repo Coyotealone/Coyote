0432ada13093494fc1247385ad0a4af4bd97a10a
Coyote <coyotealone@hotmail.fr>
2015-07-01 09:42:23 +0200

    3.2.2
    Fix errors

--- src/Coyote/SiteBundle/EventListener/LocaleListener.php
+++ src/Coyote/SiteBundle/EventListener/LocaleListener.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 namespace Coyote\SiteBundle\EventListener;
 
--- src/Coyote_/SiteBundle/Resources/public/js/script_scheduleslocked.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    var pickerOpts = { dateFormat:"yy-mm-dd" };
-    $( "#date" ).datepicker(pickerOpts);
-});
